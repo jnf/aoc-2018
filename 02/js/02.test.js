7ad406936021d67c1d2d823d34c34883f78a64ed
jnf <jnf@has-many.com>
2018-12-01 21:45:43 -0800

    adds a test that verifies solution for p1

--- 02/js/02.test.js
+++ 02/js/02.test.js
@@ -11,4 +11,15 @@ describe("compute", () => {
 
     checksum.compute(callback)
   })
+
+  test("verify p1 solution", (done) => {
+    const checksum = new Checksum("./input")
+    const expected = 8715
+    const callback = ({dubs, trips}) => {
+      expect(dubs * trips).toEqual(expected)
+      done()
+    }
+
+    checksum.compute(callback)
+  })
 })
